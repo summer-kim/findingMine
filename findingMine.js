1becdcc5bfac3a0bb7e73f12f4606ea3eeb0c1eb
summer-kim <48466707+summer-kim@users.noreply.github.com>
2020-07-31 20:28:29 +0900

    findingMineGame part3

--- findingMine.js
+++ findingMine.js
@@ -5,27 +5,29 @@ const btn = document.getElementById("excute");
 
 var dataset = [];
 var tbody = document.querySelector("tbody");
+var listForDigging = []; // position of Mine
 
 function makeArray(event) {
-  horiz = row.value;
-  verti = col.value;
+  tbody.innerHTML = "";
+  listForDigging = [];
+  verti = row.value;
+  horiz = col.value;
   numMine = mine.value;
-  randomNumberForDigging(horiz, verti, numMine);
-  for (i = 0; i < horiz; i++) {
+  for (i = 0; i < verti; i++) {
     var arr = [];
     arr.push;
     dataset.push(arr);
     const tr = document.createElement("tr");
-    for (j = 0; j < verti; j++) {
+    for (j = 0; j < horiz; j++) {
       const td = document.createElement("td");
       tr.appendChild(td);
       arr.push(1);
     }
     tbody.appendChild(tr);
   }
+  randomNumberForDigging(horiz, verti, numMine);
 }
 
-var listForDigging = [];
 function randomNumberForDigging(horiz, verti, numMine) {
   var startNum = 0; //for fill the numList
   var numList = Array(horiz * verti) //which will be used for picking up the number
@@ -40,15 +42,84 @@ function randomNumberForDigging(horiz, verti, numMine) {
     );
   }
   diggingMine(horiz, verti);
-  console.log(tbody.children[2]);
 }
 
+var mineRow = 0;
+var mineCol = 0;
 function diggingMine(horiz, verti) {
   for (i = 0; i < listForDigging.length; i++) {
-    var mineRow = Math.floor(listForDigging[i] / horiz);
-    var mineCol = (listForDigging[i] % verti) - 1;
-    console.log(mineRow, mineCol);
+    if (listForDigging[i] % horiz === 0) {
+      mineRow = Math.floor(listForDigging[i] / horiz) - 1;
+      mineCol = horiz - 1;
+    } else {
+      mineRow = Math.floor(listForDigging[i] / horiz);
+      mineCol = (listForDigging[i] % horiz) - 1;
+    }
+    tbody.children[mineRow].children[mineCol].innerHTML = "X";
+    dataset[mineRow][mineCol] = "X";
   }
-  console.log(tbody.children[2]); //tbody.children[mineRow].children[mineCol].innerHTML = "X";
+  tbody.addEventListener("contextmenu", IGuessMineIsHere);
+  tbody.addEventListener("click", checkMine);
 }
+
+function IGuessMineIsHere(e) {
+  e.preventDefault();
+  var parentNode = e.target.parentNode;
+  var whichRow = Array.prototype.indexOf.call(tbody.children, parentNode);
+  var whichCol = Array.prototype.indexOf.call(parentNode.children, e.target);
+
+  if (e.target.textContent === "" || e.target.textContent === "X") {
+    e.target.textContent = "!";
+  } else if (e.target.textContent === "!") {
+    e.target.textContent = "?";
+  } else if (e.target.textContent === "?") {
+    if (dataset[whichRow][whichCol] === "X") {
+      e.target.textContent = "X";
+    } else {
+      e.target.textContent = "";
+    }
+  }
+}
+function checkMine(e) {
+  e.preventDefault();
+  var parentNode = e.target.parentNode;
+  var whichRow = Array.prototype.indexOf.call(tbody.children, parentNode);
+  var whichCol = Array.prototype.indexOf.call(parentNode.children, e.target);
+
+  ArrAround = [
+    dataset[whichRow][whichCol - 1],
+    dataset[whichRow][whichCol + 1],
+  ];
+  if (dataset[whichRow - 1]) {
+    ArrAround = ArrAround.concat(
+      dataset[whichRow - 1][whichCol - 1],
+      dataset[whichRow - 1][whichCol],
+      dataset[whichRow - 1][whichCol + 1]
+    );
+  }
+  if (dataset[whichRow + 1]) {
+    ArrAround = ArrAround.concat(
+      dataset[whichRow + 1][whichCol - 1],
+      dataset[whichRow + 1][whichCol],
+      dataset[whichRow + 1][whichCol + 1]
+    );
+  }
+  if (e.target.textContent === "X") {
+    e.target.textContent = "퐝";
+  } else {
+    e.target.textContent = ArrAround.filter(function (x) {
+      return x === "X";
+    }).length;
+  }
+}
+
 btn.addEventListener("click", makeArray);
+
+var name = "carrot";
+function log() {
+  console.log(name);
+}
+function rename() {
+  var name = "onion";
+  log();
+}
