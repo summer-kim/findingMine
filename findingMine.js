f91c046d0e5845d3969b49aef821f41e6568b5a7
summer-kim <48466707+summer-kim@users.noreply.github.com>
2020-08-03 20:04:10 +0900

    chang the way of playing game
    let user choose the size of game array

--- findingMine.js
+++ findingMine.js
@@ -1,7 +1,6 @@
-const row = document.getElementById("row");
-const col = document.getElementById("column");
-const mine = document.getElementById("mine");
-const btn = document.getElementById("excute");
+const first = document.getElementsByClassName("stageBtn")[0];
+const second = document.getElementsByClassName("stageBtn")[1];
+const third = document.getElementsByClassName("stageBtn")[2];
 
 var dataset = [];
 const tbody = document.getElementById("tableTbody");
@@ -10,16 +9,37 @@ var listForDigging = []; // position of Mine
 var btnTbody = document.getElementById("btnTbody");
 
 var stopExecution = false;
+var verti, horiz, numMine;
+
+first.addEventListener("click", makeArray);
+second.addEventListener("click", makeArray);
+third.addEventListener("click", makeArray);
 
 function makeArray(event) {
+  if (event.target.id === "first") {
+    verti = 10;
+    horiz = 10;
+    numMine = 10;
+  } else if (event.target.id === "second") {
+    verti = 10;
+    horiz = 20;
+    numMine = 10;
+  } else {
+    verti = 15;
+    horiz = 30;
+    numMine = 10;
+  }
+  //initialize the game
+  numOpend = 0;
+  tbody.innerHTML = "";
+  stopExecution = false;
+  tbody.classList.remove("fontSize");
+  btnTbody.children[0].classList.remove("stop");
   tbody.style.display = "block";
   btnTbody.style.display = "block";
-  tbody.innerHTML = "";
-  listForDigging = [];
-  dataset = [];
-  verti = row.value;
-  horiz = col.value;
-  numMine = mine.value;
+  listForDigging = []; // array used for making random num of mine's posiion
+  dataset = []; //data of mines
+
   for (i = 0; i < verti; i++) {
     var arr = [];
     arr.push;
@@ -63,7 +83,6 @@ function diggingMine(horiz, verti) {
       mineRow = Math.floor(listForDigging[i] / horiz);
       mineCol = (listForDigging[i] % horiz) - 1;
     }
-    tbody.children[mineRow].children[mineCol].innerHTML = "X";
     dataset[mineRow][mineCol] = "X";
   }
   tbody.addEventListener("contextmenu", IGuessMineIsHere);
@@ -71,88 +90,138 @@ function diggingMine(horiz, verti) {
 }
 
 function IGuessMineIsHere(e) {
+  //when you rightclick
   e.preventDefault();
   var parentNode = e.target.parentNode;
   var whichRow = Array.prototype.indexOf.call(tbody.children, parentNode);
   var whichCol = Array.prototype.indexOf.call(parentNode.children, e.target);
 
-  if (e.target.textContent === "" || e.target.textContent === "X") {
+  if (e.target.textContent === "") {
     e.target.textContent = "!";
   } else if (e.target.textContent === "!") {
     e.target.textContent = "?";
   } else if (e.target.textContent === "?") {
-    if (dataset[whichRow][whichCol] === "X") {
-      e.target.textContent = "X";
-    } else {
-      e.target.textContent = "";
-    }
+    e.target.textContent = "";
   }
 }
+
+var numOpend = 0;
 function checkMine(e) {
+  //when you leftclick
   e.preventDefault();
   if (stopExecution) {
+    return;
+  }
+  if (e.target.textContent === "!" || e.target.textContent === "?") {
     return;
   }
   var parentNode = e.target.parentNode;
   var whichRow = Array.prototype.indexOf.call(tbody.children, parentNode);
   var whichCol = Array.prototype.indexOf.call(parentNode.children, e.target);
 
-  ArrAround = [
-    tbody.children[whichRow].children[whichCol - 1],
-    tbody.children[whichRow].children[whichCol + 1],
-  ];
-  if (tbody.children[whichRow - 1]) {
-    ArrAround = ArrAround.concat(
-      tbody.children[whichRow - 1].children[whichCol - 1],
-      tbody.children[whichRow - 1].children[whichCol],
-      tbody.children[whichRow - 1].children[whichCol + 1]
-    );
-  }
-  if (tbody.children[whichRow + 1]) {
-    ArrAround = ArrAround.concat(
-      tbody.children[whichRow + 1].children[whichCol - 1],
-      tbody.children[whichRow + 1].children[whichCol],
-      tbody.children[whichRow + 1].children[whichCol + 1]
-    );
-  }
+  var arrAround = makeArrSurround(tbody, whichRow, whichCol);
+  var arrDataAround = makeArrSurround2(dataset, whichRow, whichCol);
   var mineNumAround = 0;
-  if (e.target.textContent === "X" || e.target.textContent === "퐝") {
+
+  if (dataset[whichRow][whichCol] === "X") {
+    //when you click the mine
     e.target.style.backgroundColor = "teal";
     e.target.textContent = "퐝";
     stopExecution = true;
     tr.forEach(function (x) {
-      x.className = "stop";
+      x.className = "stop"; //to vanish the table
     });
     setTimeout(function () {
       tbody.textContent = "You Lose";
       tbody.className = "fontSize";
     }, 2000);
   } else {
-    mineNumAround = ArrAround.filter(function (x) {
-      return !!x;
-    }).filter(function (x) {
-      return x.textContent === "X";
-    }).length;
+    arrDataAround.forEach(function (value) {
+      if (value === "X") {
+        mineNumAround++;
+      }
+    });
     if (mineNumAround !== 0) {
       e.target.textContent = mineNumAround;
     }
     e.target.style.backgroundColor = "teal";
+
+    if (dataset[whichRow][whichCol] === 0) {
+      numOpend++;
+    }
     dataset[whichRow][whichCol] = 1;
-    console.log(1);
+    if (numOpend === verti * horiz - numMine) {
+      //when you open all of mines
+      stopExecution = true;
+      tr.forEach(function (x) {
+        x.className = "stop"; //to vanish the table
+      });
+      setTimeout(function () {
+        tbody.textContent = "You Win";
+        tbody.className = "fontSize";
+      }, 2000);
+    }
   }
-  if (mineNumAround === 0 && e.target.textContent !== "퐝") {
-    ArrAround.filter(function (x) {
-      return !!x;
-    }).forEach(function (around) {
-      var parentNode = around.parentNode;
-      var whichRow = Array.prototype.indexOf.call(tbody.children, parentNode);
-      var whichCol = Array.prototype.indexOf.call(parentNode.children, around);
-      if (dataset[whichRow][whichCol] === 0) {
-        return around.click();
-      }
-    });
+
+  if (mineNumAround === 0) {
+    arrAround
+      .filter(function (x) {
+        return !!x;
+      })
+      .forEach(function (around) {
+        var parentNode = around.parentNode;
+        var whichRow = Array.prototype.indexOf.call(tbody.children, parentNode);
+        var whichCol = Array.prototype.indexOf.call(
+          parentNode.children,
+          around
+        );
+        if (dataset[whichRow][whichCol] === 0) {
+          return around.click();
+        }
+      });
   }
 }
 
-btn.addEventListener("click", makeArray);
-btnTbody.addEventListener("click", makeArray);
+function makeArrSurround(element, whichRow, whichCol) {
+  Arr = [
+    element.children[whichRow].children[whichCol - 1],
+    element.children[whichRow].children[whichCol + 1],
+  ];
+
+  if (element.children[whichRow - 1]) {
+    Arr = Arr.concat(
+      element.children[whichRow - 1].children[whichCol - 1],
+      element.children[whichRow - 1].children[whichCol],
+      element.children[whichRow - 1].children[whichCol + 1]
+    );
+  }
+  if (element.children[whichRow + 1]) {
+    Arr = Arr.concat(
+      element.children[whichRow + 1].children[whichCol - 1],
+      element.children[whichRow + 1].children[whichCol],
+      element.children[whichRow + 1].children[whichCol + 1]
+    );
+  }
+  return Arr;
+}
+
+function makeArrSurround2(array, whichRow, whichCol) {
+  Arr = [array[whichRow][whichCol - 1], array[whichRow][whichCol + 1]];
+
+  if (array[whichRow - 1]) {
+    Arr = Arr.concat(
+      array[whichRow - 1][whichCol - 1],
+      array[whichRow - 1][whichCol],
+      array[whichRow - 1][whichCol + 1]
+    );
+  }
+
+  if (array[whichRow + 1]) {
+    Arr = Arr.concat(
+      array[whichRow + 1][whichCol - 1],
+      array[whichRow + 1][whichCol],
+      array[whichRow + 1][whichCol + 1]
+    );
+  }
+  return Arr;
+}
